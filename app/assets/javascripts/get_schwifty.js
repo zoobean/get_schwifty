cce99240eda7921de0b26452ab7f1ab2fabab171
Vincent Mendiola <vincent@zoobean.com>
2018-11-15 11:48:02 -0500

    Add ability to watch for DOM changes
    * Fixed bug in replaceContent method that throws an error when the oldEl.parentNode is null.
    * Added watchMeGetSchwifty method to allow the JS to be included once and have it watch for changes to the DOM to dispatch the events.

--- app/assets/javascripts/get_schwifty.js
+++ app/assets/javascripts/get_schwifty.js
@@ -1,5 +1,6 @@
 GetSchwifty = function(app) {
   _App = app;
+  _Observer = null;
 
   function dispatchEvent(ev, el, data) {
     data = data || {}
@@ -15,7 +16,10 @@ GetSchwifty = function(app) {
 
     var newContent = document.createRange().createContextualFragment(response.body);
     var newEl = newContent.firstChild;
-    oldEl.parentNode.replaceChild(newContent, oldEl);
+
+    if (oldEl.parentNode) {
+      oldEl.parentNode.replaceChild(newContent, oldEl);
+    }
 
     dispatchEvent('render:after', newEl, { schwiftyJobId: schwiftyJobId, html: response });
   }
@@ -27,6 +31,54 @@ GetSchwifty = function(app) {
   }
 
   return {
+    stopSchwifty: function() {
+      if (_Observer) {
+        _Observer.disconnect();
+      }
+    },
+    watchMeGetSchwifty: function(containerID) {
+      var targetNode = document.getElementById(containerID),
+          config = { attributes: false, childList: true, subtree: false };
+
+      // Callback function to execute when mutations are observed
+      var callback = function(mutationsList, observer) {
+          for(var mutation of mutationsList) {
+              if (mutation.type == 'childList') {
+                var el = mutation.target;
+                var schwiftyJobId = el.getAttribute('data-get-schwifty');
+                var schwiftyParams = JSON.parse(el.getAttribute('data-get-schwifty-params')) || {};
+
+                dispatchEvent('subscribe:before', el, { schwiftyJobId: schwiftyJobId, paschwiftyParamsrams: schwiftyParams });
+
+                var subscription = Object.assign({ channel: "GetSchwiftyChannel", id: schwiftyJobId }, schwiftyParams);
+
+                var cable = _App.cable.subscriptions.create(subscription, {
+                  received: function(response) {
+
+                    switch (response.status) {
+                      case 302:
+                      redirectTo(schwiftyJobId, el, response);
+                      break;
+                      default:
+                      replaceContent(schwiftyJobId, el, response);
+                    }
+
+                    cable.perform('rendered');
+                    cable.unsubscribe();
+                  }
+                });
+
+                dispatchEvent('subscribe:after', el, { schwiftyJobId: schwiftyJobId })
+              }
+          }
+      };
+
+      // Create an observer instance linked to the callback function
+      _Observer = new MutationObserver(callback);
+
+      // Start observing the target node for configured mutations
+      _Observer.observe(targetNode, config);
+    },
     showMeWhatYouGot: function(selector) {
       selector = selector || '';
 
