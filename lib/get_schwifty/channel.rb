88f141d13d835084059c7224d5991c3b5d5f44e7
David T. Allen <148455+bitlather@users.noreply.github.com>
2020-09-22 12:53:03 -0400

    Add debug output

--- lib/get_schwifty/channel.rb
+++ lib/get_schwifty/channel.rb
@@ -4,6 +4,7 @@ module GetSchwifty
   # :nodoc
   module Channel
     def subscribed
+puts '<Channel.subscribed 1>'
       reject if route.blank?
 
       stream_from channel_name
@@ -11,32 +12,39 @@ module GetSchwifty
     end
 
     def rendered
+puts '<Channel.rendered>'
       Rails.cache.write(channel_name, nil) unless GetSchwifty.allow_rerender
     end
 
     def route
+puts '<Channel.route>'
       Rails.cache.read(channel_name)
     end
 
     def controller
+puts '<Channel.controller>'
       (route.split("#").first + "_cable").camelize
     end
 
     def action
+puts '<Channel.action>'
       route.split("#").last
     end
 
     def identifiers
+puts '<Channel.identifiers>'
       connection.identifiers.collect do |key|
         [key.to_s, send(key)]
       end
     end
 
     def schwifty_job_id
+puts '<Channel.schwifty_job_id>'
       params[:id]
     end
 
     def channel_name
+puts '<Channel.channel_name>'
       "get_schwifty:#{schwifty_job_id}"
     end
   end
